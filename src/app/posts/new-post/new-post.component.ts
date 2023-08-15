45eb941e66572c311e895171a9098445af88f5a8
mustafaguler3 <mustafa.glr@hotmail.com>
2023-08-15 16:59:37 +0300

    added post image preview

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class NewPostComponent {
 
+  permalink: string = "";
+  imgSrc: any = "./assets/placeholder.png";
+  selectedImg: any;
+
+  constructor(){}
+
+  onTitleChanged($event:any){
+    const title = $event.target.value;
+    this.permalink = title.replace(/\s/g,"-")
+  }
+
+  showPreview($event:any){
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      this.imgSrc = e.target?.result;
+    }
+
+    reader.readAsDataURL($event?.target.files[0])
+    this.selectedImg = $event.target.files[0];
+  }
 }
