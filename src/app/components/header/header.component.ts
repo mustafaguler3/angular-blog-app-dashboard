72ff1158c10a9fa2362058f64b6bf77694ebabb7
mustafaguler3 <mustafa.glr@hotmail.com>
2023-08-16 18:45:59 +0300

    added login & logout query

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
+  userEmail: any;
+
+  constructor(private authService: AuthService){}
+
+  ngOnInit(): void {
+    this.userEmail = JSON.parse(localStorage.getItem('user') || '{}').email;
+  }
+
+  onLogOut(){
+    this.authService.logOut();
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Route, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
@@ -8,11 +9,28 @@ import { ToastrService } from 'ngx-toastr';
 export class AuthService {
 
   constructor(private afAuth: AngularFireAuth,
-              private toastrService:ToastrService) { }
+              private toastrService:ToastrService,
+              private router: Router) { }
 
   login(email:any,password:any){
     this.afAuth.signInWithEmailAndPassword(email,password).then(logRef => {
-      this.toastrService.success("Logged in successfully")
+      this.toastrService.success("Logged in successfully");
+      this.router.navigate(["/"])
+    }).catch(e => {
+      this.toastrService.warning(e);
+    })
+  }
+
+  loadUser(){
+    this.afAuth.authState.subscribe(user => {
+      localStorage.setItem("user",JSON.stringify(user))
+    });
+  }
+
+  logOut(){
+    this.afAuth.signOut().then(()=> {
+      this.toastrService.success("SignOut successfully")
+      this.router.navigate(["/login"])
     })
   }
 }
